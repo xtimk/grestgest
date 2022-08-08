2f3ebafebd40dcf94654a3a85201578049eb08cb
Tiziano Mele <52818668+xtimk@users.noreply.github.com>
2022-08-08 08:56:19 +0200

    add period delete feature

--- API/Controllers/PeriodController.cs
+++ API/Controllers/PeriodController.cs
@@ -47,5 +47,32 @@ namespace API.Controllers
 
             return Ok();
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) {
+            _logger.LogInformation("Requested the delete of the interval with id " + id);
+            var period_to_delete = await _context.Periods.Include(x => x.Activities).FirstOrDefaultAsync(item=> item.Id == id);
+            if(period_to_delete == null)
+            {
+                _logger.LogWarning("Requested a delete of an interval that does not exists. The id of the non-existent activity is: " + id);
+                return Ok();
+            }
+
+            if(period_to_delete.Activities.Any())
+            {
+                var activities = string.Join(", ", period_to_delete.Activities.Select(x=> x.Name).ToArray());
+                _logger.LogInformation("Requested a delete of a period that is linked to at least one activity. Linked activities: " + activities);
+                return BadRequest(new ProblemDetails(){
+                    Title = "Impossibile cancellare il periodo",
+                    Detail = "E' utilizzato dalle seguenti attivita: " + activities
+                });
+            }
+
+            _context.Periods.Remove(period_to_delete);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Successfull deleted period with id " + id);
+
+            return Ok();
+        }
     }
 }
