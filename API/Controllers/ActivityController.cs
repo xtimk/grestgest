081ac9b5d3c367741748e70506a50ec1272f325f
Tiziano Mele <52818668+xtimk@users.noreply.github.com>
2022-08-08 11:32:09 +0200

    cleanup unused imports

--- API/Controllers/ActivityController.cs
+++ API/Controllers/ActivityController.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
